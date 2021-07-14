ffc3d6688f8e61026920fdbf53c2a24b2144169b
Mark M Wandatha <marksmwangi@gmail.com>
2021-07-14 16:15:14 +0300

    Added Account

---
+++ FirstStatelessApi/Controllers/Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstStatelessApi.Controllers
+{
+    public class Account
+    {
+        public int id { get; set; }
+    }
+}
